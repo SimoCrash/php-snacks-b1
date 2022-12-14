12809623394ba5b82b271a42e8197ae9c7cb9f18
SimoCrash <simone.crescenzi@hotmail.com>
2022-12-14 17:24:05 +0100

    commit per hotwell

--- Snack1/index.php
+++ Snack1/index.php
@@ -1,5 +1,46 @@
 <?php
-
+    $arr_matches = [
+        'first_match' => [
+            'casa'=> [
+            'nome' => 'Olimpia Milano',
+            'punteggio' => '55'
+            ],
+            'ospite' => [
+            'nome' => 'Cantu',
+            'punteggio' => '60'
+            ],
+        ],
+        'second_match' => [
+            'casa'=> [
+            'nome' => 'Trieste',
+            'punteggio' => '66'
+            ],
+            'ospite' => [
+            'nome' => 'Brindisi',
+            'punteggio' => '83'
+            ],
+        ],
+        'third_match' => [
+            'casa'=> [
+            'nome' => 'Universo Treviso',
+            'punteggio' => '77'
+            ],
+            'ospite' => [
+            'nome' => 'Verona',
+            'punteggio' => '79'
+            ],
+        ],
+        'fourth_match' => [
+            'casa'=> [
+            'nome' => 'Virtus Bologna',
+            'punteggio' => '77'
+            ],
+            'ospite' => [
+            'nome' => 'Scafati',
+            'punteggio' => '84'
+            ],
+        ],
+    ]
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +52,6 @@
     <title>## Snack 1</title>
 </head>
 <body>
-    
+    <h1><?php echo array_keys($arr_matches) ?></h1>
 </body>
 </html>
